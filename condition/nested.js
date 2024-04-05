e678c7dc267dee4fb01225c521ea20d090d11646
Gama20AmTr <nntrayee@gmail.com>
2024-04-06 03:18:45 +0600

    third commit

---
+++ condition/nested.js
@@ -0,0 +1,18 @@
+const money = 0;
+
+if(money > 300){
+    console.log('Bro you are rich');
+}
+else{
+    if(money > 100){
+        console.log('tui gorib o na borolok o na');
+    }
+    else{
+        if(money > 0){
+            console.log('kola kha calcisum bara');
+        }
+        else{
+            console.log('taka chaibi na');
+        }
+    }
+}
