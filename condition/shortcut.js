e4419c3f3d59adb01dcadcb1ed8d1ff9b5d44642
Gama20AmTr <nntrayee@gmail.com>
2024-04-06 03:43:54 +0600

    repeat commit

---
+++ condition/shortcut.js
@@ -0,0 +1,20 @@
+const isLeader = true;
+// if(isLeader){
+//     console.log('leader asche! chair charo');
+// }
+
+// else{
+//     console.log('vitore dukte dibi na');
+// }
+
+// const isPassed = false;
+// if(!isPassed){
+//     console.log('tui pagol');
+// }
+
+// else{
+//     console.log('tujmi amader noyoner moni');
+// }
+
+console.log(!"cat");
+console.log(!'');
